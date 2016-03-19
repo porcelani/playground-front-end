f0e601983e3a4f5d380e4f2842cd0228bcce024f
Porcelani <porcelani@gmail.com>
2016-03-19 13:51:40 -0300

    organize jasmine test

--- appCarro/js/entity/Carro.js
+++ appCarro/js/entity/Carro.js
@@ -1,9 +1,3 @@
-/**
- * Created by Edlaine on 19/03/16.
- */
-/**
- * Created by Edlaine on 19/03/16.
- */
 var codigoCarro = 0;
 
 function Carro(fab, mod, ano, cor, pla){
@@ -15,9 +9,25 @@ function Carro(fab, mod, ano, cor, pla){
     this.ano = ano;
     this.cor = cor;
     this.placa = (function(str){
-        return str.toUpperCase();
+        //return str.toUpperCase();
+        return str;
     })(pla);
+
     this.toString = function(){
         return this.fabricante + ' ' + this.modelo + ' ' + this.ano + ' ' + this.cor + ' (' + this.placa + ')';
     }
+
+    this.ligar = function(){
+        return "Carro Ligado";
+    }
 }
+
+//Padrão ECMA-6
+//'use strict';
+//
+//class Carro2{
+//...
+//ligar(){
+//    return "Carro Ligado";
+//}
+//}
--- appCarro/js/entity/Carro2.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-class Carro2{
-    
-   ligar(){
-        return "Carro Ligado";
-    }
-}
