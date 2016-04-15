aea13de9d91a2a3156efdf99f0e8b95fe183f7cb
Porcelani <porcelani@gmail.com>
2016-04-15 06:33:37 -0300

    -files dist

--- appCarro/dist/js/utils.js
+++ appCarro/dist/js/utils.js
@@ -1,1 +1,1 @@
-/*! appcarro - v1.0.0 - 2016-04-10 */var Util=function(){var a={};return a.replaceWithData=function(a,b){for(var c in b)if(b.hasOwnProperty(c)){var d="{{"+c+"}}",e=new RegExp(d,"g");a.innerHTML=a.innerHTML.replace(e,b[c])}},a}();
+/*! appcarro - v1.0.0 - 2016-04-15 */var Util=function(){var a={};return a.replaceWithData=function(a,b){for(var c in b)if(b.hasOwnProperty(c)){var d="{{"+c+"}}",e=new RegExp(d,"g");a.innerHTML=a.innerHTML.replace(e,b[c])}},a}();
